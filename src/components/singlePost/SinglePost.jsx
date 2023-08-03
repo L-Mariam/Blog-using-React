cebdcfa9dbbd7d79b2a4b7482257703b33faf354
L-Mariam <l.mariam.here@gmail.com>
2023-08-04 01:43:15 +0400

    Second commit

--- src/components/singlePost/SinglePost.jsx
+++ src/components/singlePost/SinglePost.jsx
@@ -10,7 +10,7 @@ export default function SinglePost() {
        <h1 className="singlePostTitle"> Lorem ipusm die soloeeyr sp
        <div className="singlePostEdit">
        <i className="singlePostIcon fa-solid fa-pen-to-square"></i>
-       <i className="singlePostIcon fa-regular fa-trash"></i>
+       <i className="singlePostIcon fa fa-trash"></i>
        </div>
 
         </h1>
@@ -18,10 +18,12 @@ export default function SinglePost() {
             <span className="singlePostAuthor"> Author: <b> Lena </b></span>
             <span className="singlePostDate"> 1 hour ago</span>
         </div>
-       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora soluta ipsa placeat, perferendis nesciunt iusto deleniti expedita quasi velit libero distinctio blanditiis ipsam praesentium voluptates in temporibus hic eos. Debitis?
+       <p className="singlePostDesc">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora soluta ipsa placeat, perferendis nesciunt iusto deleniti expedita quasi velit libero distinctio blanditiis ipsam praesentium voluptates in temporibus hic eos. Debitis?
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque sunt nemo deleniti, provident autem consectetur quia veniam ipsum, quae ab enim tempore libero nam? Enim soluta esse veniam repellendus natus.
        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus ab autem dicta quaerat aliquam eius accusantium impedit atque. Aperiam iste sint velit harum molestiae est ea vel aspernatur dolor veniam.
        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint repellat, consequatur nulla excepturi eius dignissimos doloremque nobis sapiente laborum fugiat velit minima officiis placeat asperiores illum temporibus adipisci aspernatur repellendus.
+       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus ab autem dicta quaerat aliquam eius accusantium impedit atque. Aperiam iste sint velit harum molestiae est ea vel aspernatur dolor veniam.
+
        </p>
         </div>
     </div>
